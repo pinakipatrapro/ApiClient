6b78988ba9a4ae463d9a95e798e05b3ffda8cd03
Pinaki Patra <pinaki.patra.pro@gmail.com>
2018-01-28 01:10:45 +0530

    Add Message For Deletion Success

--- ApiExplorer/WebContent/controller/ViewDataOperations.js
+++ ApiExplorer/WebContent/controller/ViewDataOperations.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function (Controller,MessageToast) {
 	"use strict";
 
 	var ViewDataOperations = Controller.extend("sap.pinaki.controller.ViewDataOperations", {
@@ -13,12 +14,30 @@ sap.ui.define([
 		oModel.remove(bindingPath, {
 			method: "DELETE",
 			success: function (data) {
-				alert("success");
+				var msg = 'Entry deleted successfully';
+				MessageToast.show(msg);
 			},
 			error: function (e) {
-				alert("error");
+				var msg = 'Error while deleting entry';
+				MessageToast.show(msg);
 			}
 		});
-	}
+	};
+	ViewDataOperations.editEntry = function (oEvent) {
+		var source = oEvent.getSource();
+		var bindingPath = source.getBindingContext().getPath();
+		var oModel = source.getModel();
+		oModel.remove(bindingPath, {
+			method: "DELETE",
+			success: function (data) {
+				var msg = 'Entry deleted successfully';
+				MessageToast.show(msg);
+			},
+			error: function (e) {
+				var msg = 'Error while deleting entry';
+				MessageToast.show(msg);
+			}
+		});
+	};
 	return ViewDataOperations;
 });
